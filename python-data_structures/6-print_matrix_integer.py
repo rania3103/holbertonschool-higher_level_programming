3e418b0ddc4bf609b9af91f8baa89a70ec5936b5
rania3103 <raniaabassi33@gmail.com>
2023-10-03 11:31:31 +0100

    fix output

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    i = 0
     for row in matrix:
         i = 0
         for val in row:
             i += 1
             if i != len(row):
                 print("{:d} ".format(val), end="")
-            print("{:d}".format(val), end="")
+            else:
+                print("{:d}".format(val), end="")
         print()
