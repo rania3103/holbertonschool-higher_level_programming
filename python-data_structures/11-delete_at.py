09da08893d0b039802c0fde83abf7b06b3047edb
rania3103 <raniaabassi33@gmail.com>
2023-10-04 09:15:14 +0100

    delete an elemnt at a specific position

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,6 +2,10 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
+    elif idx == 0:
+        return (my_list[1:])
+    elif (idx == len(my_list) - 1):
+        return (my_list[:-1])
     else:
         new = my_list.copy()
         new = new[:idx] + new[idx + 1:]
