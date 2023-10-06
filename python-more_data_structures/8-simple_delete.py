9ce550564312182fd47ab67e16943b0d839f8518
rania3103 <raniaabassi33@gmail.com>
2023-10-06 20:07:53 +0100

    deletes a key in a dictionary.

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary.keys():
+    if key not in a_dictionary.keys():
+        return
+    else:
         del (a_dictionary[key])
         return (a_dictionary)
-    else:
-        return
