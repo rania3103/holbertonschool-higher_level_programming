683f1865fed627c60e075f1078edbe61b6add575
rania3103 <raniaabassi33@gmail.com>
2023-10-08 08:43:38 +0100

    prints x elements of a list.

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+        print()
+        return x
+    except BaseException:
+        list_len = sum(1 for i in my_list)
+        print()
+        return list_len
