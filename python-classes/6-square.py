ba9831af4079544480e3fc869895f98384d1ffc7
rania3103 <raniaabassi33@gmail.com>
2023-10-09 15:09:00 +0100

    check the output

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
     @position.setter
     def position(self, value):
         """set the value for position"""
-        test = [isinstance(numb, int) and numb >= 0 for numb in value]
+        test = all([isinstance(numb, int) and numb >= 0 for numb in value])
         if len(value) != 2 or not test:
             raise TypeError("position must be a tuple of 2 positive integers")
 
@@ -53,3 +53,18 @@ class Square:
                 for col in range(self.__size):
                     print("#", end="")
                 print()
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, ('a', 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
