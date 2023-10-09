09ab7ee4c0a73c385285855809635f66cacd1e43
rania3103 <raniaabassi33@gmail.com>
2023-10-09 11:48:33 +0100

    create square a class with privante instance attribute and display error message if the size is invalid

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,13 +5,13 @@
 class Square():
     """this is a class named Square"""
 
-    def __init__(self, __size=0):
+    def __init__(self, size=0):
         """
         initialize size otherwise displaying error message if the value of size is invalid
         """
-        if not isinstance(__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif __size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = __size
+            self.__size = size
