24dcbec8a3614fe9e8ef79302e2a74a41ab37e03
rania3103 <raniaabassi33@gmail.com>
2023-10-09 10:16:43 +0100

    create  an empty class Square

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """
+     this is  an empty class Square that defines a square
+    """
+    pass
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
