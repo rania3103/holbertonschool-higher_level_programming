fd095543b2f83cd3a21f91ea93cd99b574de75c4
rania3103 <raniaabassi33@gmail.com>
2023-10-09 10:52:57 +0100

    create a class with private attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""create a class Square that defines a square"""
+"""create a class Square that defines a square with a private instance size"""
 
 
-class Square:
-    """
-    this is a class named square
-    attributes:
-    __size(no type/value verification):Private instance attribute
-    """
-    __size = None
+class Square():
+    """this is a class named square"""
+
+    def __init__(self, __size):
+        """Instantiation with size (no type/value verification)"""
+        self.__size = __size
