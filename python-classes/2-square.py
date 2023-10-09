4bd240a83528a86d582530c32889ac4e91fbc192
rania3103 <raniaabassi33@gmail.com>
2023-10-09 12:12:47 +0100

    create square a class with privante instance attribute and display error message if the size is invalid

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python3
+"""create a class Square that defines a square with a private instance size"""
+
+
+class Square():
+    """this is a class named square"""
+
+    def __init__(self, __size):
+        """Instantiation with size (no type/value verification)"""
+        
+        self.__size = __size
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/usr/bin/python3
 """ create a class that defines Square and control the atrribute size"""
 
 
