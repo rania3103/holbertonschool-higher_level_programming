4da1a6897f5c3d788ae6ae0614210b9dbd2fdf99
rania3103 <raniaabassi33@gmail.com>
2023-10-11 18:27:06 +0100

    print full name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a function that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ print full name or an error msg in case of error"""
+    if not (isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    elif not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
