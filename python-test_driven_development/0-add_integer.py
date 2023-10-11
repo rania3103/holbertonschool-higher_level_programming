e318a42fc5eb08e88292966fb0dfcaf9005bcfd0
rania3103 <raniaabassi33@gmail.com>
2023-10-11 14:25:59 +0100

    add documentation to the function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-    """ display an message if there is an error otherwise add a + b"""
+    """ display a message if there is an error otherwise add a + b"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
