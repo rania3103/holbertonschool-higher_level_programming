e7edc3b4254526e1405dd7b1e7edfa1658b2273b
rania3103 <raniaabassi33@gmail.com>
2023-10-11 19:32:55 +0100

    fix autopep8

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """" a function that prints a square with the character #"""
+
+
 def print_square(size):
+    """print square with # or an error msg in case of error"""
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
@@ -12,5 +15,5 @@ def print_square(size):
     else:
         for i in range(size):
             for j in range(size):
-                print("#", end ="")
+                print("#", end="")
             print()
