49309f47c28c925b01f321ce6fa51fdc2f019b11
rania3103 <raniaabassi33@gmail.com>
2023-10-12 09:47:57 +0100

    fix 2 new lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,9 +12,16 @@ def text_indentation(text):
         res = ""
         for i in range(len(text)):
             if text[i] == '.' or text[i] == '?' or text[i] == ':':
-                res += text[i] + "\n\n\n"
-                res.strip()
+                res += text[i] + "\n\n"
             else:
                 res += text[i]
-                res.strip()
         print(res, end="")
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
