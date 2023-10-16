c73e7b88116844a8a55e779ed6435b0b1fae4cca
rania3103 <raniaabassi33@gmail.com>
2023-10-16 16:14:07 +0100

    check if the object is an instance of a specified class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Write a function that returns True if the object
-is exactly an instance of the specified
-class ; otherwise False."""
+"""Write a function that returns True 
+if the objectis exactly an instance 
+of the specifiedclass ; otherwise False."""
 
 
 def is_same_class(obj, a_class):
-    """ this is the function"""
+    """ this is the function and it returns True or False"""
     if type(obj) is  a_class:
         return True
     else:
