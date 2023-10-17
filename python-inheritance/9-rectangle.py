3e197f0897cb43fe846ac5319699a57eb7b3f487
rania3103 <raniaabassi33@gmail.com>
2023-10-17 13:33:21 +0100

    add area and __str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that inherits from BaseGeometry"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ this is the class"""
+
+    def __init__(self, width, height):
+        """initialize height and width"""
+
+        if self.integer_validator("width", width):
+            self.__width = width
+        if self.integer_validator("height", height):
+            self.__height = height
+
+    def area(self):
+        """calculate the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ return the following rectangle description:
+        [Rectangle] <width>/<height>"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
