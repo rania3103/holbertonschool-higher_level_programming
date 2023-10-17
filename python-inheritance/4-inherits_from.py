fb19fa3abe3d7c962fd25469b714ce08cb801a3c
rania3103 <raniaabassi33@gmail.com>
2023-10-17 11:42:10 +0100

    update description of the function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,8 +6,8 @@ specified class ; otherwise False."""
 
 
 def inherits_from(obj, a_class):
-    """ this the function"""
-    if type(obj) == a_class:
+    """ this is the function"""
+    if type(obj) is a_class:
         return False
     elif isinstance(obj, a_class):
         return True
