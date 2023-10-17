6750512db87a711ecb3ad2aa641c2d22c13d47fb
rania3103 <raniaabassi33@gmail.com>
2023-10-17 11:39:20 +0100

    only sub class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a function that returns True if the object
+is an instance of a class that inherited
+(directly or indirectly) from the
+specified class ; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """ this the function"""
+    if isinstance(obj, a_class):
+        return False
+    elif isinstance(obj, a_class):
+        return True
