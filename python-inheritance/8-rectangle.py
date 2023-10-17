cc7e7aab5a41213aca380d26668ae5caca347f1e
rania3103 <raniaabassi33@gmail.com>
2023-10-17 13:43:36 +0100

    delete if condition

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """initialize height and width"""
 
-        if self.integer_validator("width", width):
-            self.__width = width
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
