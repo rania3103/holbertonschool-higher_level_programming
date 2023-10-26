d287b2fa6548837b2cbf5b27f5f24df3010c68bf
rania3103 <raniaabassi33@gmail.com>
2023-10-26 14:36:33 +0100

    fix pep8 in __str__

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,5 +92,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """[Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
