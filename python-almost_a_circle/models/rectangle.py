6a65055a19e3b139aa2304259af937021189c211
rania3103 <raniaabassi33@gmail.com>
2023-10-26 14:59:25 +0100

    update display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Write the class Rectangle that inherits from Base"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -85,7 +85,13 @@ class Rectangle(Base):
 
     def display(self):
         """display the rectangle"""
+        if self.__y > 0:
+            for i in range(self.__y):
+                print()
         for i in range(self.__height):
+            if self.__x > 0:
+                for i in range(self.__x):
+                    print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
