8d9ca65b543d60318727a74483741679456ac5fe
rania3103 <raniaabassi33@gmail.com>
2023-10-30 11:53:47 +0100

    add condition to_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,8 +18,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None:
+        """returns the JSON string representation
+        of list_dictionaries"""
+        if len(list_dictionaries) == 0 or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
