88aaeb02b9f8157ad1c92be5746c0b501a2bd592
rania3103 <raniaabassi33@gmail.com>
2023-11-04 19:04:09 +0100

    replace elif with if

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -36,11 +36,11 @@ class Square(Rectangle):
         if len(args) != 0:
             if len(args) >= 1:
                 self.id = args[0]
-            elif len(args) >= 2:
+            if len(args) >= 2:
                 self.size = args[1]
-            elif len(args) >= 3:
+            if len(args) >= 3:
                 self.x = args[2]
-            elif len(args) >= 4:
+            if len(args) >= 4:
                 self.y = args[3]
         else:
             for k, v in kwargs.items():
