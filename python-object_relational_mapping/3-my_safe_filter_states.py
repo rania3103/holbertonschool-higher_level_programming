0fe73090dd8b555d38263d699161f9032c2f4cb8
rania3103 <raniaabassi33@gmail.com>
2023-11-27 14:59:09 +0100

    prevent SQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument."""
+from MySQLdb import connect
+from sys import argv
+
+
+if __name__ == "__main__":
+    """conncet to db and run the query then print the result"""
+    db = connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id;",(argv[4],))
+    res = cur.fetchall()
+    for row in res:
+        print(row)
