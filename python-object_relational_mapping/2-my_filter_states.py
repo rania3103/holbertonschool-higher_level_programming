3146752bae2fd6e7618a921421ba546d8436d83f
rania3103 <raniaabassi33@gmail.com>
2023-11-27 11:41:05 +0100

    convert it into a standalone script

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""" a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
+""" a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument."""
 from MySQLdb import connect
 from sys import argv
 
 
-def filter_info_by_inp(username, pwd, dbname, statename):
+if __name__ == "__main__":
     """conncet to db and run the query then print the result"""
     db = connect(
         host="localhost",
-        user=username,
-        password=pwd,
-        database=dbname,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
         port=3306
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(statename))
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(argv[4]))
     res = cur.fetchall()
     for row in res:
         print(row)
-
-
-if __name__ == "__main__":
-    filter_info_by_inp(argv[1], argv[2], argv[3], argv[4])
