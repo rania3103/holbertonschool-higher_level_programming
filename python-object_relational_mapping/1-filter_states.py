491498c628ee450e9a29bc1e90c2a61d68596b34
rania3103 <raniaabassi33@gmail.com>
2023-11-27 11:37:46 +0100

    pep8

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 from MySQLdb import connect
 from sys import argv
 
@@ -18,4 +19,3 @@ if __name__ == "__main__":
     res = cur.fetchall()
     for row in res:
         print(row)
-    db.close()
