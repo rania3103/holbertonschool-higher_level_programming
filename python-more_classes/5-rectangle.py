b9b8f8c406fd4c8b844ff1ab55193c1cbda08794
Rania Abassi <135613385+rania3103@users.noreply.github.com>
2023-11-28 15:28:45 +0100

    Update 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -4,7 +4,7 @@ and 2 public instance methods and print rectangle"""
 
 
 class Rectangle:
-    """this is the class Rectangle"""
+    """this is the class Rectangle """
 
     def __init__(self, width=0, height=0):
         """initialize width and height"""
